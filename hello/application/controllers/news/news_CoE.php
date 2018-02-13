063ad94c06ccd7c0f666d4f48006539077f0803f
EMAILPSU\5730213036 <saranyuu88@gmail.com>
2018-02-13 21:23:27 +0700

    update ui admin

---
+++ hello/application/controllers/news/news_CoE.php
@@ -0,0 +1,22 @@
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+	
+	class news_CoE extends CI_Controller {
+		function __construct()
+	    {
+	        // this is your constructor
+	        parent::__construct();
+	       
+	    }
+		public function new_s()
+		{
+			$this->load->view('css');
+			$this->load->view('top-bar');
+			$this->load->view('right-sidebar-admin');
+			$this->load->view('new_s/new_s');
+			$this->load->view('footer');
+			$this->load->view('script');
+		}
+		
+}
+?>
