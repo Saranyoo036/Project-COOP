180a71f0969a48fd3f8a5f9245c82b3faf048058
EMAILPSU\5730213036 <saranyuu88@gmail.com>
2018-04-05 01:31:37 +0700

    add position

--- application/views/Add/Add_Po.php
+++ application/views/Add/Add_Po.php
@@ -4,41 +4,75 @@
 		<div class="block-header">
 			<h2>หน้าแรก</h2>
 			<ul class="breadcrumb">
-				<li class="breadcrumb-item"><a href="index.html">หน้าแรก</a></li>			
-				<li class="breadcrumb-item active">หน้าแรก</li>
+				<li class="breadcrumb-item"><a href="index.html">Add</a></li>			
+				<li class="breadcrumb-item active">Add Job position</li>
 			</ul>
 		</div>
 		<div class="row clearfix">
 			<div class="col-md-12 col-lg-12">
 				<div class="card visitors-map">
 					<div class="header">
-						<h2>CONTENT</h2>
+						<h2>Add Job position</h2>
 					</div>
 					<div class="body">
-						<div class="row">							
-							<div class="col-sm-12">	
-								<p class="indent">CONTENT</p>
-							</div>
-	
-						</div>
+
+						<div class="row clearfix">
+                            <div class="col-sm-12">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" class="form-control">
+                                        <label class="form-label">Position</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" class="form-control">
+                                        <label class="form-label">Skill </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" class="form-control">
+                                        <label class="form-label">Coordination</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" class="form-control">
+                                        <label class="form-label">Contact</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" class="form-control">
+                                        <label class="form-label">Email </label>
+                                    </div>
+                                </div>
+                            </div>
+                            
+						</div></br>
+
+						<div class="button-demo">
+		                            
+		                    <button type="button" class="btn  btn-raised btn-success waves-effect">Save Information</button>
+		                            
+		                </div>
+						
+
+
 					</div>
 				</div>
 			</div>
 		</div>
-		
-		<div class="row clearfix">
-			<div class="col-sm-12 col-md-12">
-				<div class="card">
-					<div class="body" id="footer">
-							<div class="col-sm-12">
-								<p class="copy m-b-0">© Copyright
-									<time class="year">2017</time>
-									WrapTheme  - All Rights Reserved</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+
 		</div>
 	</div>
 </section>
