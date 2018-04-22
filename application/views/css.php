0978980a78c9dd11aba38658cdcd4d1088c2d5f6
EMAILPSU\5730213036 <saranyuu88@gmail.com>
2018-04-22 22:43:25 +0700

    head

--- application/views/css.php
+++ application/views/css.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-<title>COOP
+<title>COOP Application System
 </title>
 <link rel="icon" href="<?php echo base_url();?>favicon.ico" type="image/x-icon">
 <link href="<?php echo base_url();?>assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
